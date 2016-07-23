76dbc3a8384a6d678980337489e371e80e91d2cb
Leandro M Barreto <Leandro.SaBarreto@hotmail.com>
2016-07-23 03:58:01 -0300

    unit tests for Reply.NoError(value)

--- source/MlIB.Reply.Tests.Unit/Features/Reply_NoError.cs
+++ source/MlIB.Reply.Tests.Unit/Features/Reply_NoError.cs
@@ -9,20 +9,41 @@ namespace MlIB.Reply.Tests.Unit.Features
 
         //public static Reply<T> NoError<T>(T value)
 
-        //I:valid
-        //O:
+        //I:null value
+        //O:has no error
+        //O:null as value
+        [TestMethod]
+        public void Reply_NoError_value_null()
+        {
+            var result = M.Reply.NoError<string>(null);
 
-        //I:valid exception
-        //O:
+            Assert.IsFalse(result.HasError, "WHY HAS ERROR??");
+            Assert.AreEqual(null, result.Value, "WHY NOT EXPECTED VALUE??");
+        }
 
-        //I:null
-        //O:
+        //I:valid value
+        //O:has no error
+        //O:expected valid value
+        [TestMethod]
+        public void Reply_NoError_value_valid()
+        {
+            var result = M.Reply.NoError(Stubs.Common.VAL_anyValue);
 
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
+            Assert.IsFalse(result.HasError, "WHY HAS ERROR??");
+            Assert.AreEqual(Stubs.Common.VAL_anyValue, result.Value, "WHY NOT EXPECTED VALUE??");
+        }
 
-        //}
+        //I:valid exception value
+        //O:has no error
+        //O:exception as value
+        [TestMethod]
+        public void Reply_NoError_value_exception()
+        {
+            var result = M.Reply.NoError(Stubs.Common.EXCEPTION);
+
+            Assert.IsFalse(result.HasError, "WHY HAS ERROR??");
+            Assert.AreEqual(Stubs.Common.EXCEPTION, result.Value, "WHY NOT EXPECTED VALUE??");
+        }
 
     }
 }
